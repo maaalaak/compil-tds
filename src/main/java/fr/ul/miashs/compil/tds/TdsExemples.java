e18cb5bd6a4bff4e90cd3859b68b6cf1bf274bc9
maaalaak <malakmelhaoui79@gmail.com>
2026-02-07 21:16:04 +0100

    etape6: ajout des TDS jusqu'au programme 6

--- src/main/java/fr/ul/miashs/compil/tds/TdsExemples.java
+++ src/main/java/fr/ul/miashs/compil/tds/TdsExemples.java
@@ -1,61 +1,80 @@
 package fr.ul.miashs.compil.tds;
 
-public class TdsExemples {
+public class TdsExemples  {
 
+    // Programme 1
     public static Tds prog1() {
+
         Tds tds = new Tds();
-        tds.ajouter(new Item("main", "void", "fonction"));
+
+        tds.ajouter(new Item("main", "void", "fonction", "global"));
+
         return tds;
     }
 
-    public static void main(String[] args) {
-        Tds tds = prog1();
-        tds.afficher();
-    }
+    // Programme 2
     public static Tds prog2() {
 
-    Tds tds = new Tds();
+        Tds tds = new Tds();
 
-    tds.ajouter(new Item("main", "void", "fonction"));
+        tds.ajouter(new Item("main", "void", "fonction", "global"));
 
-    tds.ajouter(new Item("i", "int", "global"));
-    tds.ajouter(new Item("j", "int", "global"));
-    tds.ajouter(new Item("k", "int", "global"));
-    tds.ajouter(new Item("l", "int", "global"));
+        tds.ajouter(new Item("i", "int", "global", "global"));
+        tds.ajouter(new Item("j", "int", "global", "global"));
+        tds.ajouter(new Item("k", "int", "global", "global"));
+        tds.ajouter(new Item("l", "int", "global", "global"));
 
-    return tds;
-}
-public static Tds prog3() {
+        return tds;
+    }
 
-    Tds tds = new Tds();
+    // Programme 3
+    public static Tds prog3() {
 
-    tds.ajouter(new Item("main", "void", "fonction"));
+        Tds tds = new Tds();
 
-    tds.ajouter(new Item("z", "int", "global"));
-    tds.ajouter(new Item("a", "int", "global"));
-    tds.ajouter(new Item("b", "int", "global"));
+        tds.ajouter(new Item("main", "void", "fonction", "global"));
 
-    return tds;
-}
-public static Tds prog4() {
+        tds.ajouter(new Item("z", "int", "global", "global"));
+        tds.ajouter(new Item("a", "int", "global", "global"));
+        tds.ajouter(new Item("b", "int", "global", "global"));
 
-    Tds tds = new Tds();
+        return tds;
+    }
 
-    tds.ajouter(new Item("main", "void", "fonction"));
+    // Programme 4
+    public static Tds prog4() {
 
-    tds.ajouter(new Item("res", "int", "global"));
+        Tds tds = new Tds();
 
-    return tds;
-}
-public static Tds prog5() {
+        tds.ajouter(new Item("main", "void", "fonction", "global"));
+        tds.ajouter(new Item("res", "int", "global", "global"));
 
-    Tds tds = new Tds();
+        return tds;
+    }
 
-    tds.ajouter(new Item("main", "void", "fonction"));
+    // Programme 5
+    public static Tds prog5() {
 
-    tds.ajouter(new Item("res", "int", "global"));
+        Tds tds = new Tds();
 
-    return tds;
-}
+        tds.ajouter(new Item("main", "void", "fonction", "global"));
+        tds.ajouter(new Item("res", "int", "global", "global"));
 
-}
+        return tds;
+    }
+
+    // Programme 6
+    public static Tds prog6() {
+
+        Tds tds = new Tds();
+
+        tds.ajouter(new Item("main", "void", "fonction", "global"));
+
+        tds.ajouter(new Item("f", "int", "fonction", "global"));
+
+        tds.ajouter(new Item("a", "int", "param", "f"));
+        tds.ajouter(new Item("b", "int", "param", "f"));
+        tds.ajouter(new Item("res", "int", "local", "f"));
+
+        return tds;
+    }}
